981385db2b097362fabff73365176b0f47c05b0d
Patilrohit45 <patilrohit.official@gmail.com>
2024-08-26 00:44:47 +0530

    initial commit

---
+++ SnapTalk/SnapTalk/SnapTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapTalkApp.swift
+//  SnapTalk
+//
+//  Created by Rohit Patil on 26/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
